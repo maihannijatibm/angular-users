eff12328c37f749dea06b07bd4823788e2a0165b
Maihan Nijat <maihan.nijat@ibm.com>
2023-09-23 02:50:28 -0400

    feat: add routing for registration module and add registration module as lazy loading module in app.module.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./registration/registration.module').then(m => m.RegistrationModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
