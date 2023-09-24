368512f9e994eea21c7752589cd9375b06d19a69
Maihan Nijat <maihan.nijat@ibm.com>
2023-09-24 00:40:58 -0400

    feat: update app constants structure and add profile route to it. Align UserService constants with app.constants new structure.

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,5 +1,10 @@
 export const APP_CONSTANTS = {
-  USER_REGISTRATION_URL:
-    'https://mocki.io/v1/7f434df6-a4ac-4817-ab7c-dd39a564d01d',
-  GET_USER_URL: 'https://mocki.io/v1/611a3036-4420-48a5-b8da-9b461853cdd2',
+  URLS: {
+    USER_REGISTRATION:
+      'https://mocki.io/v1/7f434df6-a4ac-4817-ab7c-dd39a564d01d',
+    GET_USER: 'https://mocki.io/v1/611a3036-4420-48a5-b8da-9b461853cdd2',
+  },
+  ROUTES: {
+    profile: 'profile'
+  }
 };
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
 import { APP_CONSTANTS } from '../app.constants';
@@ -10,11 +10,17 @@ import { User } from '../interfaces/user';
 export class UserService {
   constructor(private readonly httpClient: HttpClient) {}
 
-  create(): Observable<any> {
-    return this.httpClient.get(APP_CONSTANTS.USER_REGISTRATION_URL);
+  create(user: User): Observable<any> {
+    const params = new HttpParams()
+      .set('name', user.name)
+      .set('email', user.email)
+      .set('bio', user.bio);
+    return this.httpClient.get(APP_CONSTANTS.URLS.USER_REGISTRATION, {
+      params,
+    });
   }
 
   get(): Observable<User> {
-    return this.httpClient.get<User>(APP_CONSTANTS.GET_USER_URL);
+    return this.httpClient.get<User>(APP_CONSTANTS.URLS.GET_USER);
   }
 }
