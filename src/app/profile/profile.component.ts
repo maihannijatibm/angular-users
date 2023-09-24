838e459a03d44d5d89874f0c9950c8acb52d1544
Maihan Nijat <maihan.nijat@ibm.com>
2023-09-24 09:40:34 -0400

    feat: fetch user profile from API and display it in UI. Use success handler to handler the success result and error handler for handling errors.

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs/internal/Subscription';
+import { User } from '../interfaces/user';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit, OnDestroy {
+  user?: User;
 
+  isLoading: boolean = false;
+
+  error: boolean = false;
+
+  subscriptions: Subscription[] = [];
+
+  constructor(private readonly userService: UserService) {}
+
+  ngOnInit(): void {
+    this.getProfile();
+  }
+
+  getProfile(): void {
+    this.isLoading = true;
+    this.error = false;
+
+    const subscription = this.userService.get().subscribe({
+      next: (result) => this.successHandler(result),
+      error: (error) => this.errorHandler(error),
+      complete: () => (this.isLoading = false),
+    });
+
+    this.subscriptions.push(subscription);
+  }
+
+  successHandler(user: User): void {
+    this.user = user;
+  }
+
+  errorHandler(error: any): void {
+    this.error = true;
+    console.log(error);
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach((sub) => sub.unsubscribe);
+  }
 }
--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfileComponent } from './profile.component';
 import { RouterModule, Routes } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
 
 const routes: Routes = [
   {
@@ -12,6 +13,6 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [ProfileComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [CommonModule, RouterModule.forChild(routes), MatCardModule],
 })
 export class ProfileModule {}
