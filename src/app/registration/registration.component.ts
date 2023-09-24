2ba08368ad3b379630799c215d830975143a7acd
Maihan Nijat <maihan.nijat@ibm.com>
2023-09-23 21:26:47 -0400

    feat: create user registration form using ReactiveForm and FormBuilder. The form has three controls with validation rules.

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.scss']
+  styleUrls: ['./registration.component.scss'],
 })
-export class RegistrationComponent {
+export class RegistrationComponent implements OnInit {
+  form?: FormGroup;
 
+  controlNames = {
+    name: 'name',
+    email: 'email',
+    bio: 'bio',
+  };
+
+  controlMaxLengths = {
+    name: 64,
+    email: 64,
+    bio: 256,
+  };
+
+  constructor(private readonly formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.createForm();
+  }
+
+  createForm() {
+    this.form = this.formBuilder.group({
+      name: [
+        '',
+        [
+          Validators.required,
+          Validators.maxLength(this.controlMaxLengths.name),
+        ],
+      ],
+      email: [
+        '',
+        [Validators.email, Validators.maxLength(this.controlMaxLengths.email)],
+      ],
+      bio: [
+        '',
+        [Validators.required, Validators.maxLength(this.controlMaxLengths.bio)],
+      ],
+    });
+  }
 }
