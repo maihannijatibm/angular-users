b0495e5a5717bec39ea0505347142da2a4bfda20
Maihan Nijat <maihan.nijat@ibm.com>
2023-09-23 23:54:05 -0400

    feat: create a constant file for storing static values.

---
+++ src/app/app.constants.ts
@@ -0,0 +1,5 @@
+export const APP_CONSTANTS = {
+  USER_REGISTRATION_URL:
+    'https://mocki.io/v1/7f434df6-a4ac-4817-ab7c-dd39a564d01d',
+  GET_USER_URL: 'https://mocki.io/v1/611a3036-4420-48a5-b8da-9b461853cdd2',
+};
