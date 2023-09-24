c69ea9bf32c115899bd42142fcd04992b9f3de75
Maihan Nijat <maihan.nijat@ibm.com>
2023-09-23 23:55:44 -0400

    feat: add HttpClientModule dependency for HttpClient class.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     AppRoutingModule,
     StoreModule.forRoot({}, {}),
   ],
