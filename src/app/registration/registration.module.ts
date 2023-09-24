343bad9c25e7feed9750c4a8e907cc6715bee0de
Maihan Nijat <maihan.nijat@ibm.com>
2023-09-23 21:28:24 -0400

    feat: add Angular Material modules (input, button and card) in the registration module.

--- src/app/registration/registration.module.ts
+++ src/app/registration/registration.module.ts
@@ -2,21 +2,27 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegistrationComponent } from './registration.component';
 import { RouterModule, Routes } from '@angular/router';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 const routes: Routes = [
   {
     path: '',
-    component: RegistrationComponent
-  }
+    component: RegistrationComponent,
+  },
 ];
 
 @NgModule({
-  declarations: [
-    RegistrationComponent
-  ],
+  declarations: [RegistrationComponent],
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
-  ]
+    RouterModule.forChild(routes),
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+  ],
 })
-export class RegistrationModule { }
+export class RegistrationModule {}
