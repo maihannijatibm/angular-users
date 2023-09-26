f680061beafabb7eea851de8337b494e6d9c5459
Maihan Nijat <maihan.nijat@ibm.com>
2023-09-25 21:01:45 -0400

    feat: write unit test case for auth guard.

--- src/app/guards/auth-guard.guard.spec.ts
+++ src/app/guards/auth-guard.guard.spec.ts
@@ -1,17 +1,56 @@
 import { TestBed } from '@angular/core/testing';
-import { CanActivateFn } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivateFn,
+  RouterStateSnapshot,
+} from '@angular/router';
 
 import { authGuardGuard } from './auth-guard.guard';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { UserState } from '../store/reducers/reducer';
+import * as UserActions from '../store/actions/actions';
+import { StoreModule } from '@ngrx/store';
+import { of } from 'rxjs/internal/observable/of';
 
 describe('authGuardGuard', () => {
-  const executeGuard: CanActivateFn = (...guardParameters) => 
-      TestBed.runInInjectionContext(() => authGuardGuard(...guardParameters));
+  const executeGuard: CanActivateFn = (...guardParameters) =>
+    TestBed.runInInjectionContext(() => authGuardGuard(...guardParameters));
+
+  let mockStore: MockStore<UserState>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [StoreModule.forRoot()],
+      providers: [
+        provideMockStore({
+          initialState: {
+            user: undefined,
+            registration: {
+              success: true,
+              error: false,
+            },
+          },
+        }),
+      ],
+    });
+
+    mockStore = TestBed.inject(MockStore);
   });
 
   it('should be created', () => {
     expect(executeGuard).toBeTruthy();
+  });
+
+  it('should return true if registration is successful', () => {
+    const route: ActivatedRouteSnapshot = {} as any;
+    const state: RouterStateSnapshot = {} as any;
+
+    mockStore.dispatch(UserActions.userRegistrationSuccess({ success: true }));
+
+    const storeSpy = spyOn(mockStore, 'pipe').and.returnValue(of(true));
+
+    executeGuard(route, state);
+
+    expect(storeSpy).toHaveBeenCalled();
   });
 });
--- src/app/guards/auth-guard.guard.ts
+++ src/app/guards/auth-guard.guard.ts
@@ -1,6 +1,6 @@
 import { inject } from '@angular/core';
 import { CanActivateFn } from '@angular/router';
-import { Store } from '@ngrx/store';
+import { Store, select } from '@ngrx/store';
 import { selectUserRegistration } from '../store/selectors/selectors';
 import { map } from 'rxjs';
 import * as UserActions from '../store/actions/actions';
@@ -8,7 +8,9 @@ import * as UserActions from '../store/actions/actions';
 export const authGuardGuard: CanActivateFn = () => {
   const store = inject(Store);
 
-  return store.select(selectUserRegistration).pipe(
+  const registration$ = store.pipe(select(selectUserRegistration));
+
+  return registration$.pipe(
     map((result) => {
       if (!result.success) {
         store.dispatch(UserActions.navigateToRegistration());
